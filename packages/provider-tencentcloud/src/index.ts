424e7109dfdd5edbf8006e49de30f5aa727e792d
Chen Peng <chen56@msn.com>
2025-05-19 10:40:14 +0800

    去掉commonjs支持，都moudle

---
+++ packages/provider-tencentcloud/src/index.ts
@@ -0,0 +1,18 @@
+/**
+ * 请通过此模块导入腾讯云相关API，避免直接从子文件导入。
+ * 正确范例: import { Vpc } from 'pa/providers/tencent_cloud/vpc'
+ * 错误范例: import { Vpc } from 'pa/providers/tencent_cloud/vpc/vpc' */
+/**
+ * @packageDocumentation
+ * 腾讯云服务模块
+ *
+ * @example
+ * import { Vpc } from '@qpa/providers-tencentcloud';
+ */
+// export * from './provider';
+// export * from './factory';
+// export * from './vpc';
+
+
+export * from '@/providers/tencent_cloud/index.ts';
+
