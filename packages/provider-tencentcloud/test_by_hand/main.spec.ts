5da7696579e072bec72895142c6b7ed2c91f75d7
Chen Peng <chen56@msn.com>
2025-05-27 23:07:33 +0800

    增加 腾讯云vpc apply unit test

--- packages/provider-tencentcloud/test_by_hand/main.spec.ts
+++ packages/provider-tencentcloud/test_by_hand/main.spec.ts
@@ -1,44 +1,10 @@
 // noinspection JSUnusedAssignment,PointlessBooleanExpressionJS
 
 import {afterEach, beforeEach, describe, expect, it} from "vitest";
-import {TencentCloud} from "../src/factory.ts";
-import {Project} from "@qpa/core";
-import * as dotenv from 'dotenv';
-import * as dotenvExpand from 'dotenv-expand';
-import {VpcState} from "@qpa/provider-tencentcloud";
-// 首先加载 .env 文件中的原始键值对
-// dotenv.config() 返回一个包含 parsed 属性的对象，其中是解析后的键值对
-const myEnv = dotenv.config();
-// 然后使用 dotenvExpand.expand() 来处理变量扩展
-// 它会修改 process.env，并返回一个包含所有扩展后变量的对象
-dotenvExpand.expand(myEnv);
+import {VpcState} from "../src/index.ts";
+import {TextFixture} from "./fixture.ts";
 
-class TextFixture {
-  readonly project = Project.of({name: "unit_test"});
-  readonly tc = TencentCloud.createFactory(this.project, {
-    credential: {
-      secretId: process.env.TENCENTCLOUD_SECRET_ID!,
-      secretKey: process.env.TENCENTCLOUD_SECRET_KEY!,
-    },
-  });
-  static of(): TextFixture {
-    return new TextFixture();
-  }
-
-  async before() {
-    await this.project.destroy();
-    expect(this.project.resourceInstances.length).toBe(0);
-    await this.project.refresh();
-    expect(this.project.resourceInstances.length).toBe(0);
-  }
-  async after() {
-
-  }
-
-
-}
-
-describe('vpc apply', () => {
+describe('vpc', () => {
   const fixture = TextFixture.of();
   const {tc, project} = fixture;
 
@@ -49,7 +15,7 @@ describe('vpc apply', () => {
     await fixture.after();
   })
 
-  it('Eager mode destroy', async () => {
+  it('vpc apply', async () => {
     await project.apply(async _ => {
       const vpc = await tc.vpc.vpc({
         name: "vpc1",
